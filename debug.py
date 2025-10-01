5ccbef35ea14fb3765752f6f107fb3797414bdf3
Victorious Ngaruiya <victoriousngaruiya@gmail.com>
2025-10-02 00:28:09 +0300

    added to debug.py

--- debug.py
+++ debug.py
@@ -1,3 +1,22 @@
-from customer import customer
-from coffee import coffee
-from purchase import purchase
+from customer import Customer
+from coffee import Coffee
+from order import Order
+
+
+#starter objects
+c1 = Customer("Alice")
+c2 = Customer("Bob")
+
+latte = Coffee("Latte")
+espresso = Coffee("Espresso")
+
+c1.create_order(latte, 3.5)
+c1.create_order(espresso, 4.0)
+c2.create_order(latte, 5.0)
+
+print (c1.name)
+print (b1.)
+
+
+ipdb.set_trace()
+
