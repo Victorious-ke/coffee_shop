6283905dfb2dff6e1c8b23b445221f9548ed2325
Victorious Ngaruiya <victoriousngaruiya@gmail.com>
2025-10-02 07:33:16 +0300

    added functions

--- coffee.py
+++ coffee.py
@@ -16,3 +16,22 @@ class Coffee:
      @name.setter
     def name(self, _):
         raise Exception("Coffee name cannot be changed after creation")
+
+    def orders(self):
+        return [order for order in Order.all if order.coffee is self]
+
+    def customers(self):
+        return list({order.customer for order in self.orders()})
+
+    def num_orders(self):
+        return len(self.orders())
+
+    def average_price(self):
+        orders = self.orders()
+        if not orders:
+            return 0
+        return sum(order.price for order in orders) / len(orders)
+
+
+
+        
